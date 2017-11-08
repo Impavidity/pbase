ef981247670f83212a7e3751863696da933cec68
Peng <spone.2014@gmail.com>
2017-11-08 16:57:49 -0500

    rename

--- setup.py
+++ setup.py
@@ -1,1 +1,14 @@
+from setuptools import find_packages, setup
 
+
+setup(
+    name='pbase',
+    version='0.0.1',
+    author='Peng Shi',
+    author_email='peng_shi@outlook.com',
+    description='framework for deep learning applications',
+    url='https://github.com/Impavidity/pbase',
+    license='MIT',
+    install_requires=['tqdm'],
+    packages=find_packages(),
+)
