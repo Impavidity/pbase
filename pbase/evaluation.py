9441f40ed40534940697b96c58a2e04aa73eb758
Shi Peng <spone.2014@gmail.com>
2017-12-30 12:19:08 -0500

    add class for optimization, integrate mrr/map evaluation

--- pbase/evaluation.py
+++ pbase/evaluation.py
@@ -1,4 +1,6 @@
 from collections import Counter
+import os
+import subprocess
 
 def get_span(label, with_type):
     span, tag = [], []
@@ -63,6 +65,53 @@ def NEREvaluation(goldLabel, predLabel, with_type=False):
         f1 = 2 * precision * recall / (precision + recall)
     return precision, recall, f1
 
+def MRR(pairs):
+    """
+    :param pairs:  A list of pair. One pair is a query. == (score, sim)
+    :return: mean_reciprocal_rank
+    """
+    pass
 
+def MAP(pairs):
+    """
+    :param pairs:  A list of pair. One pair is a query. == (score, sim)
+    :return: mean_average_precision
+    """
+    pass
+
+def precision_top_k(pairs, k):
+    """
+    :param pairs: A list of pair. One pair is a query. == (score, sim)
+    :param k: precision on top k
+    :return: P_k
+    """
+    pass
+
+def TREC_MAP_MRR(pairs, pred_fname="pred.txt", gold_fname="gold.txt"):
+    qrel_fname = gold_fname
+    results_fname = pred_fname
+    qrel_template = '{qid} 0 {docno} {rel}\n'
+    results_template = "{qid} 0 {docno} 0 {sim} biaffine\n"
+    with open(qrel_fname, 'w') as f1, open(results_fname, 'w') as f2:
+        qid = 0
+        for pair in pairs:
+            qid += 1
+            docno = 0
+            for predicted, actual in zip(pair[0], pair[1]):
+                docno += 1
+                f1.write(qrel_template.format(qid=qid, docno=docno, rel=actual))
+                f2.write(results_template.format(qid=qid, docno=docno, sim=predicted))
+
+    trec_eval_path = '/mnt/collections/p8shi/dev/biaffine/qa/trec_eval-9.0.5/trec_eval'
+    trec_out = subprocess.check_output([trec_eval_path, '-m', 'map', '-m', 'recip_rank', qrel_fname, results_fname])
+    trec_out_lines = str(trec_out, 'utf-8').split('\n')
+    mean_average_precision = float(trec_out_lines[0].split('\t')[-1])
+    mean_reciprocal_rank = float(trec_out_lines[1].split('\t')[-1])
+
+    os.remove(qrel_fname)
+    os.remove(results_fname)
+
+
+    return mean_average_precision, mean_reciprocal_rank
 
 
