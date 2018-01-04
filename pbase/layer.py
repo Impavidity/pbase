e814e53020f3fc3cba8b062d86deda242d3c8915
Peng <spone.2014@gmail.com>
2018-01-04 08:45:27 -0500

    add sequence cnn

--- pbase/layer.py
+++ pbase/layer.py
@@ -72,7 +72,7 @@ class LSTM(nn.LSTM):
 class SimpleCNN(nn.Module):
     def __init__(self, num_of_conv, in_channels, out_channels, kernel_size, in_features, out_features, stride=1,
                  dilation=1, groups=1, bias=True, active_func = F.relu, pooling=F.max_pool1d,
-                 dropout=0.5):
+                 dropout=0.5, padding_strategy="default", padding_list=None):
         """
 
         :param num_of_conv: Follow kim cnn idea
@@ -86,16 +86,19 @@ class SimpleCNN(nn.Module):
         if len(kernel_size) != num_of_conv:
             print("Number of kernel_size should be same num_of_conv")
             exit(1)
+        if padding_list == None:
+            if padding_strategy == "default":
+                padding_list = [(k_size-1, 0) for k_size in kernel_size]
 
         self.conv = nn.ModuleList([nn.Conv2d(in_channels=in_channels,
                                              out_channels=out_channels,
                                              kernel_size=(k_size, in_features),
                                              stride=stride,
-                                             padding=(k_size-1, 0),
+                                             padding=padding,
                                              dilation=dilation,
                                              groups=groups,
                                              bias=bias)
-                                   for k_size in kernel_size])
+                                   for k_size, padding in zip(kernel_size, padding_list)])
         self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(num_of_conv * out_channels, out_features)
         self.pooling = pooling
@@ -112,6 +115,42 @@ class SimpleCNN(nn.Module):
         x = self.dropout(x)
         x = self.fc(x)
         return x
+
+class SequenceCNN(nn.Module):
+    def __init__(self, num_of_conv, in_channels, out_channels, kernel_size, in_features, stride=1,
+                 dilation=1, groups=1, bias=True):
+        super(SequenceCNN, self).__init__()
+        if type(kernel_size) == int:
+            kernel_size = [kernel_size]
+        if len(kernel_size) != num_of_conv:
+            print("Number of kernel_size should be same num_of_conv")
+            exit(1)
+        for k_size in kernel_size:
+            if k_size % 2 == 0:
+                print("The kernel size is better to be odd")
+                exit(1)
+        padding_list = [(int(k_size / 2), 0) for k_size in kernel_size]
+        self.conv = nn.ModuleList([nn.Conv2d(in_channels=in_channels,
+                                             out_channels=out_channels,
+                                             kernel_size=(k_size, in_features),
+                                             stride=stride,
+                                             padding=padding,
+                                             dilation=dilation,
+                                             groups=groups,
+                                             bias=bias)
+                                   for k_size, padding in zip(kernel_size, padding_list)])
+
+    def forward(self, input):
+        if len(input.size()) == 3:
+            input = input.unsqueeze(1)
+        # input = (batch, in_channels, sent_len, word_dim)
+        x = [conv(input).squeeze(3).transpose(1,2) for conv in self.conv]
+        # x = (batch, sent_len, out_channels) * Ks
+        x = torch.cat(x, dim=2)
+        return x
+
+
+
 
 class CharCNN(SimpleCNN):
     """
